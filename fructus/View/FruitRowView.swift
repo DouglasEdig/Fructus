a12e2d956d38ff1a44ab89e4d5a15437e7dfdbb0
Douglas Silva <douglas.edig@outlook.com>
2024-06-13 23:24:56 -0300

    feat: add list fructus and logic button

---
+++ fructus/View/FruitRowView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitRowView.swift
+//  fructus
+//
+//  Created by Douglas Matheus da Silva on 13/06/24.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitRowView()
+}
