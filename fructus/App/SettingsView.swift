67597c60d1df0a08fac8660ec2f5d08e3ac510c1
Douglas Silva <douglas.edig@outlook.com>
2024-06-15 22:37:56 -0300

    feat: add settings and finish app

---
+++ fructus/App/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  fructus
+//
+//  Created by Douglas Matheus da Silva on 15/06/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
---
+++ fructus/View/SettingsLabelView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsLabelView.swift
+//  fructus
+//
+//  Created by Douglas Matheus da Silva on 15/06/24.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsLabelView()
+}
